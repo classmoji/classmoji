0be726575638ff8d2b285082c0cf43a23e50dd58
Muhammad Moiz <muhammad.moiz.26@dartmouth.edu>
2026-05-27 11:26:15 -0400

    chore(webapp): remove /modules redirect splat routes
    Old /modules URLs were never deployed for this refactor; the splat
    redirects and helper are dead code.

--- apps/webapp/app/routes/admin.$class.modules.$.tsx
+++
@@ -1,6 +0,0 @@
-import type { LoaderFunctionArgs } from 'react-router';
-import { redirectModulesToRepos } from '~/utils/redirects.server';
-
-export async function loader({ params, request }: LoaderFunctionArgs) {
-  return redirectModulesToRepos('admin', params.class, params['*'], request);
-}
